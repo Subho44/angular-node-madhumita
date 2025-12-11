c2e259e503a3f1b8120e7944113c8a4918f63bcb
Subho44 <98902921+Subho44@users.noreply.github.com>
2025-12-11 11:15:04 +0530

    ok
    ok

---
+++ Netbanking-system/client/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {ReactiveFormsModule,FormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Netbanking-system/client/src/app/services/auth.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import axios from 'axios';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private API_URL = 'http://localhost:5600/api/auth';
+
+  constructor() { }
+
+  async register(data:{name:string;email:string,password:string}) {
+    return axios.post(`${this.API_URL}/register`,data);
+  }
+  async login(data:{email:string,password:string}) {
+  const res = await axios.post(`${this.API_URL}/login`,data);
+  const token = res.data.token;
+
+  if(token) {
+    localStorage.setItem('token',token);
+  }
+  return res;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+
+  getToken():string |null {
+    return localStorage.getItem('token');
+  }
+  isLoggedin():boolean {
+    return !!this.getToken();
+  }
+
+   async getDashboard() {
+    const token = this.getToken();
+
+    return axios.get(`${this.API_URL}/dashboard`,{
+      headers:{
+        Authorization:`Bearer ${token}`
+      }
+    });
+  }
+
+
+}
