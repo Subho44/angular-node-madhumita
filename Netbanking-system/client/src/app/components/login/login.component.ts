6919d65b4727af99b8f33326b0d430d10d845299
Subho44 <98902921+Subho44@users.noreply.github.com>
2025-12-13 11:38:33 +0530

    done
    done

--- Netbanking-system/client/src/app/components/login/login.component.ts
+++ Netbanking-system/client/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,39 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  'loginForm':FormGroup;
+  loading = false;
+  error='';
+  success='';
+
+   constructor(
+    private fb:FormBuilder,
+    private auth:AuthService,
+    private router:Router
+
+  ) {}
+
+  ngOnInit(): void {
+   this.loginForm = this.fb.group({
+    email:['',[Validators.required,Validators.email]],
+    password:['',Validators.required],
+
+  });
+  }
+  async onSubmit() {
+    if(this.loginForm.invalid) {
+      return;
+    }
+    this.loading = true;
+    this.error = '';
+    this.success = '';
+
+    try {
+      await this.auth.login(this.loginForm.value as any);
+       this.router.navigate(['/dashboard']);
+    } catch(err:any){
+      this.error = err;
+    }
+  }
 
 }
--- Netbanking-system/client/src/app/components/register/register.component.ts
+++ Netbanking-system/client/src/app/components/register/register.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  'registerForm':FormGroup;
+  loading = false;
+  error='';
+  success='';
+
+  constructor(
+    private fb:FormBuilder,
+    private auth:AuthService,
+    private router:Router
+
+  ) {}
+
+  ngOnInit(): void {
+   this.registerForm = this.fb.group({
+    name:['',Validators.required],
+    email:['',[Validators.required,Validators.email]],
+    password:['',[Validators.required,Validators.minLength(6)]],
+
+  });
+  }
+   
+
+  async onSubmit() {
+    if(this.registerForm.invalid) {
+      return;
+    }
+    this.loading = true;
+    this.error = '';
+    this.success = '';
+
+    try {
+      await this.auth.register(this.registerForm.value as any);
+      this.success = 'Registration Success';
+      setTimeout(()=> this.router.navigate(['/login']),1000);
+    } catch(err:any){
+      this.error = err;
+    }
+  }
 
 }
